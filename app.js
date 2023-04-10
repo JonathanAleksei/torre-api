b282fe175aee1545cf5ff789647618d001e37aac
JonathanAleksei <j.suarez.a.garcia@gmail.com>
2023-04-10 15:01:32 -0500

    adding default router

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var cors = require('cors')
 app.use(cors())
 const usersRoutes = require('./api/routes/users');
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "proxy server live ready"
+    })
+})
 app.use('/users', usersRoutes);
 
 module.exports = app;
